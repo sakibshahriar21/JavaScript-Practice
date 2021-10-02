a1cca2f6fedcb67fa05df3b407e7468a872b1e26
sakibcse570 <17201122@uap-bd.edu>
2021-10-02 21:09:27 +0600

    JS file created, all JS code separated from html and connected with html file, js file moved in JS folder

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+
+document.getElementById("root").innerHTML = "I love JavaScript!"; 
+document.getElementById("idh1").innerHTML = "I'm H1";
+document.getElementById("idh3").innerHTML = "I'm H3"; 
+
+console.log("I am learning javascript!");
+console.log("JS is cool!");
+document.write("JS file is connected");
+window.alert("This is window.alert() method ");
+
+

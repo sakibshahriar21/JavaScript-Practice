5646cc0bc53557167be3f8cd2cc7e3744e222bd6
sakibcse570 <17201122@uap-bd.edu>
2021-10-02 22:41:45 +0600

    variables and constants practice file added

---
+++ JS/variables_and_constants.js
@@ -0,0 +1,23 @@
+var num1 = 22;
+let num2 = 100;
+/*Javascript is a dynamic typing language, so we do not need to spicify
+varible types when deaclaring*/
+var addition = num1 + num2;
+console.log(addition); 
+
+//variable declaration 
+
+var $sakib; //valid 
+var _num; //valid
+var _ab23b; //valid
+var MyName; //valid
+var myName; //both are different because JS is case sensitive
+//var 2b; INVALID 
+
+const pi = 3.1416; //value can not be re-assigned!
+console.log(pi);
+addition += pi;
+console.log(addition);
+
+
+

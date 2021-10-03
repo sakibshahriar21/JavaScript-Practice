48720b3f2c213fce034e561ca29ccc159ed0e1d6
sakibcse570 <17201122@uap-bd.edu>
2021-10-03 20:56:23 +0600

    Fahrenheit to Celcius temperature conversion

---
+++ JS/Temperature_Conversion.js
@@ -0,0 +1,6 @@
+var temp_celcius = prompt("Enter the temperature in celcius: ");
+var fahrenheit;
+
+fahrenheit = 9 / 5 * temp_celcius + 32;
+window.alert("Fahrenheit: " + fahrenheit + " Degree");
+console.log("Task Complete");

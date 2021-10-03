dec361ca1fca03140221df992e5ce7e82dca15ae
sakibcse570 <17201122@uap-bd.edu>
2021-10-03 20:31:01 +0600

    Assignment Operators

---
+++ JS/Assignment_operators.js
@@ -0,0 +1,21 @@
+var a = 19;
+var b = 12;
+a = b;
+console.log(a);
+
+c = a + b;
+console.log(c);
+
+console.log(a /= 12);
+console.log(a %= 3);
+
+var string = "Hello ";
+string = string + "World";
+console.log(string);
+
+string += '!';
+console.log(string);
+
+var num = 2;
+num **= 2; //power assignment 
+console.log(num);

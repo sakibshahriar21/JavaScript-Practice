50fa883b7241fc9eccec3ee4b352c3f325c78301
sakibcse570 <17201122@uap-bd.edu>
2021-10-03 20:13:29 +0600

    comparison, logical and conditional operators

---
+++ JS/comparison_logical_conditional_operators.js
@@ -0,0 +1,15 @@
+console.log(4 == '4');
+console.log(4 === '4');
+console.log(4 !== 4);
+console.log(4 !== '4');
+
+var a = (5>4);
+console.log(a);
+
+console.log(true || true || false);
+
+var a = (1 < 8) ? "Hello" : "World";
+console.log(a);
+
+var a = (8 < 5) ? "Hello" : "World";
+console.log(a);

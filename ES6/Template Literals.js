1694862ac5f1e529bfd2df3ae58c122a74ac0c2d
sakibcse570 <17201122@uap-bd.edu>
2021-10-05 22:41:19 +0600

    Template Literals

---
+++ ES6/Template Literals.js
@@ -0,0 +1,25 @@
+console.log("First line \nSecond line");
+
+console.log(`First line
+Second line`); //backtick
+
+let a = `First line
+Second line`;
+console.log(a);
+
+let age = 36;
+console.log("His age is: " + age);
+console.log(`His age is: ${age}`); 
+
+let Name = "Sakib";
+ age = "22";
+let place = "Dhaka";
+console.log(`His name is ${Name}
+His age is ${age}
+He is from ${place}`);
+
+let A = 34;
+let B = 67; 
+console.log(`The result is = ${A+B} `);
+console.log(`${A} + ${B} = ${A+B} `);
+

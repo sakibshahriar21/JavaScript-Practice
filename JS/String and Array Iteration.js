51b2445581966a250b450b2432833de8612c3649
sakibcse570 <17201122@uap-bd.edu>
2021-10-08 21:04:07 +0600

    JS String and Array iteratioon

---
+++ JS/String and Array Iteration.js
@@ -0,0 +1,13 @@
+let name = "Asia Pacific";
+let food = ["Cake", "Chocolate", "Icecream"]
+//console.log(name[0]);
+
+for(var i = 0; i<name.length; i++){
+    console.log(`Index: ${i}`);
+    console.log(name[i]);
+}
+
+for(var i = 0; i<food.length; i++){
+    console.log(`Index: ${i}`);
+    console.log(food[i]);
+}

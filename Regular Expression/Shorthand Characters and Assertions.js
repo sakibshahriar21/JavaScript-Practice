799ae29151651739784235c8fcf561c604ae52d1
sakibcse570 <17201122@uap-bd.edu>
2021-10-18 02:10:14 +0600

    JS RE Shorthand Characters and Assertions

---
+++ Regular Expression/Shorthand Characters and Assertions.js
@@ -0,0 +1,56 @@
+let re;
+let str;
+
+
+// Shorthand Character Classes
+
+re = /\w/; //Word character-> alpha numeric, Word character matches single character
+//re = /w/; //checking only for w , literal character
+re = /\w+/; //one or more alpha numeric character
+re = /\W/; //Non Word Character, matches single character
+re = /\W+/; //One or more Non Word Character
+re = /\d/; //Single digit character check
+re = /\d+/; // One or more digit charater check
+re = /\D/; //Single non digit character check [ for non digit only]
+re = /\D+/; //One or more non digit character check [ for non digit only]
+re = /\s/; //Match white space
+re = /\S/; //Match non white space
+re = /\b/; //word boundary
+re = /Hello\b/; //means only Hello x x will be accepted, x = characters
+re =/\bHello\b/; //starts with specific word and then whitespaces allwed
+
+//Assertions
+
+re = /x(?=y)/; //Matches x only if x is before y
+re = /x(?=yz)/; 
+
+re = /x(?!yz)/;//matchtes x only if x is not before yz
+
+str = "+8801600000000";
+str = "(#&^";
+str = "(#&v^"; 
+str = "h14214hbkk#";
+str = "02141654";
+str = "55 54";
+str = "8988";
+str = " ";
+str = "Hello Sakib";
+str = "Hello21 Sakib";
+str = "Hello 21 Sakib";
+
+str ="xyyyy";
+str ="xyz";
+str = "sdxgsyzdgg";
+
+
+console.log(re.exec(str));
+reTest(re, str);
+
+function reTest(re, str){
+    if(re.test(str)){
+        console.log(`'${str}' matches '${re.source}'`);
+    }
+    else{
+        console.log(`'${str}' doesn't matches '${re.source}'`);
+    }
+}
